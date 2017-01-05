08e2381a4ec53d9ef70a8f829db14adc89d9b3ad
Mark Harrison <mark@mivok.net>
2017-01-04 22:41:52 -0500

    Example script

---
+++ example.lua
@@ -0,0 +1,94 @@
+#!/usr/bin/env simplecli
+-- vim: ft=lua
+
+function banner()
+    -- This is printed when the app starts, and can be used to explain the
+    -- purpose of the cli and print any useful information at startup.
+    -- E.g. "S3 Client"
+    return "Example CLI"
+end
+
+function prompt()
+    -- Example of a dynamic prompt
+    return os.date("%H:%M:%S") .. "> "
+end
+
+function do_helloworld(cmd, args)
+    -- Simple hello world command, prints out the first argument
+    io.write("Hello world: ", cmd, " - ", args[1], "\n")
+end
+
+function do_myvar(cmd, args)
+    -- Get/Set a string variable
+    -- These variables are accessible as global variables in lua, and you can
+    -- predeclare them to get default values (otherwise the default is nil)
+    cli_variable("myvar", args[1])
+end
+
+function do_profile(cmd, args)
+    -- Set/get environment variables
+    cli_envvar("AWS_PROFILE", args[1])
+end
+
+function do_debug(cmd, args)
+    -- Boolean globals
+    cli_toggle("debug_mode")
+end
+
+function do_cmd(cmd, args)
+    -- Run an external command - just use os.execute
+
+    -- Set a default for the first arg
+    args[1] = args[1] or "world"
+
+    -- Note: if you don't set a default arg, it will be nil and there will be
+    -- an error if you try to concatenate it. You can do the following do get
+    -- around the issue:
+    -- os.execute("somecommand " .. tostring(args[1])) -- gives "nil"
+    -- os.execute("somecommand " .. (args[1] or "")) -- gives ""
+
+    -- The command is executed by the shell, so you can do pipelines here
+    os.execute("echo Hello " .. args[1] .. " | sed s/foo/bar/")
+end
+
+function do_edit(cmd, args)
+    -- The following is how you do an edit workflow, where a command downloads
+    -- a file, you edit it in your text editor and then it's re-uploaded if it
+    -- was modified.
+    tempfile = os.tmpname()
+    os.execute(string.format("curl -s -o %s httpbin.org/get?foo=hello%%20world",
+        tempfile))
+
+    -- Edit the file, return if it was changed, and print a message if it
+    -- wasn't.
+    modified = cli_edit(tempfile)
+
+    if modified then
+        os.execute(string.format([[
+            curl -X POST -H 'Content-type: application/json' \
+                httpbin.org/post -d @%s
+            ]], tempfile))
+    end
+    os.remove(tempfile)
+end
+
+function do_cat(cmd, args)
+    -- Display a downloaded file
+    tempfile = os.tmpname()
+
+    -- This is contrived (you could just not add the -o option to curl), but
+    -- it's intended to show the behavior where the command you run downloads
+    -- the file to disk (e.g. aws s3 cp)
+    os.execute("curl -s -o " .. tempfile .. " https://www.example.com/")
+
+    -- cat the file
+    f = io.open(tempfile)
+    content = f:read("*all")
+    io.write(content)
+    f:close()
+    -- Or you can do one of the following using os.execute:
+    -- os.execute("cat " .. tempfile)
+    -- os.execute("less " .. tempfile)
+
+    os.remove(tempfile)
+end
